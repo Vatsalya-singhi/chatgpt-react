755d57f89b12a627f7dc4f1948e5a9b5e742197a
Vatsalya-singhi <vatsalya.singhi4316@gmail.com>
2023-01-15 04:15:38 +0530

    copy to clipboard feature

--- src/App.js
+++ src/App.js
@@ -62,6 +62,13 @@ const Delete = () => (
     </svg>
 )
 
+const Copy = () => (
+    <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#fff">
+        <path fillRule="evenodd" clipRule="evenodd" d="M4 4l1-1h5.414L14 6.586V14l-1 1H5l-1-1V4zm9 3l-3-3H5v10h8V7z" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M3 1L2 2v10l1 1V2h6.414l-1-1H3z" />
+    </svg>
+)
+
 export default function App() {
 
     const chatContainer = document.querySelector('#chat_container');
@@ -89,6 +96,13 @@ export default function App() {
                         </button>
                     </div>
                     <div className="message" id={uniqueId}> {value} </div>
+
+                    {isAI &&
+                        (<div>
+                            <button onClick={() => dispactCommand("copyToClipboard", value)}>
+                                <Copy />
+                            </button>
+                        </div>)}
                 </div>
             </div>
         )
